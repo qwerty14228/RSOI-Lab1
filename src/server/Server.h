682110b77c20bc2fdd3df31f98fb14dee6e12164
qwerty14228 <shapranmatvey@gmail.com>
2024-11-12 20:13:51 +0300

    добавили post ручку

--- src/server/Server.h
+++ src/server/Server.h
@@ -22,43 +22,76 @@ public:
     }
 
     void run() {
-        this->server.add_route("/persons/").get([this](const auto& req, auto& res){
-            boost::json::array array;
+        this->server
+            .add_route("/api/v1/persons")
+            .get([this](const auto& req, auto& res){
+                boost::json::array array;
 
-            for (auto &person : this->logic->getAll()) {
-                array.push_back(boost::json::object{
+                for (auto &person : this->logic->getAll()) {
+                    array.push_back(boost::json::object{
+                        {"id", person->id},
+                        {"name", person->name},
+                        {"work", person->work},
+                        {"address", person->address},
+                        {"age", person->age}
+                    });
+                }
+
+                res.body() = boost::json::serialize(array);
+                res.set_header(beast::http::field::content_type, "application/json");
+            })
+            .post([this](const auto& req, auto& res) {
+                auto body = req.body();
+                auto json_body = boost::json::parse(body);
+
+                if (
+                    !json_body.is_object() ||
+                    !json_body.contains("name") ||
+                    !json_body.contains("work") ||
+                    !json_body.contains("address") ||
+                    !json_body.contains("age")
+                ) {
+                    throw beauty::http_error::client::bad_request("Bad Request");
+                }
+
+                auto name = json_body["name"].as<std::string>();
+                auto work = json_body["work"].as<std::string>();
+                auto address = json_body["address"].as<std::string>();
+                auto age = json_body["age"].as<int>();
+
+                auto person = this->logic->Create(name, work, address, age);
+
+                if (!person) {
+                    throw beauty::http_error::client::bad_request("Bad Request");
+                }
+
+                res.result(beast::http::status::created);
+            
+                res.set_header(beast::http::field::location, "/api/v1/persons/" + std::to_string(person->id));
+            });
+
+        this->server
+            .add_route("/api/v1/persons/:id")
+            .get([this](const auto& req, auto& res) {
+                auto id = req.a("id").as_integer();
+
+                auto person = this->logic->getById(id);
+
+                if (!person) {
+                    throw beauty::http_error::client::not_found("Not found");
+                }
+
+                boost::json::object person_object = {
                     {"id", person->id},
                     {"name", person->name},
                     {"work", person->work},
                     {"address", person->address},
                     {"age", person->age}
-                });
-            }
+                };
 
-            res.body() = boost::json::serialize(array);
-            res.set_header(beast::http::field::content_type, "application/json");
-        });
-
-        this->server.add_route("/person/:id").get([this](const auto& req, auto& res) {
-            auto id = req.a("id").as_integer();
-
-            auto person = this->logic->getById(id);
-
-            if (!person) {
-                throw beauty::http_error::client::not_found("Not found");
-            }
-
-            boost::json::object person_object = {
-                {"id", person->id},
-                {"name", person->name},
-                {"work", person->work},
-                {"address", person->address},
-                {"age", person->age}
-            };
-
-            res.body() = boost::json::serialize(person_object);
-            res.set_header(beast::http::field::content_type, "application/json");
-        });
+                res.body() = boost::json::serialize(person_object);
+                res.set_header(beast::http::field::content_type, "application/json");
+            });
 
         this->server.listen(this->port);
 
